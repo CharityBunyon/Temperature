05523848c2d1ec9557d67f28b3d29af1ea99f250
Charity Bunyon <cseymou5@gmail.com>
2019-09-15 12:47:59 -0500

    added converter unction

--- main.js
+++ main.js
@@ -14,20 +14,25 @@ const printToDom = (divID, textToPrint) => {
     selectedDiv.innerHTML = textToPrint;
 }
 
-const convertToCelsius = () => {
+// Celsius Function
+const convertToCelsius = (x) => {
     const x = tempInput.value;
-    const convertToC = (x -32) * 5/9;
-    const ftemp = convertToC.toFixed();
-    if (ftemp < 0) {
+    const convertToC = (x - 32) * 5/9;
+    const ctemp = convertToC.toFixed();
+    if (ctemp < 0) {
         convertedTemp.style.color = 'blue';
-    } else if (ftemp > 32) {
+    } else if (ctemp > 32) {
         convertedTemp.style.color = 'red';
     } else {
         convertedTemp.style.color = 'green';
     }
+    printToDom(ctemp + '\xBOC', 'output')
+    
 };
 
-const toFahrenheit = () => {
+
+//Fahrenheit Function
+const convertToFahrenheit = (x) => {
     const x = tempInput.value;
     const convertToF = (x * 9/5) + 32;
     const ftemp = convertToF.toFixed();
@@ -38,6 +43,7 @@ const toFahrenheit = () => {
     } else {
         convertedTemp.style.color = 'green';
     }
+    printToDom(ftemp + '\xBOF', 'output')
 };
 
 //Get a reference to the button element in the DOM
@@ -46,9 +52,16 @@ const button = document.getElementById('converter');
 //This function should determine which conversion should happen based on which radio button us selected
 
 const determineConverter = (e) => {
-    console.log("event", e);
+    if (celsiusRadioBtn.checked === true) {
+        tempInput.focus();
+        convertToCelsius();
+       
+    } else {
+        (fahRadioBtn.checked === true)
+        tempInput.focus();
+        convertToFahrenheit();
+    }
 };
-
 
 //Assign a function to be executed when the button is clicked
 button.addEventListener('click', determineConverter);
